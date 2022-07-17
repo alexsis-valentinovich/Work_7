54b4d371d4c5aaa5fcadffc531f9aa36d68b8954
alexsis-valentinovich <alexsis-valentinovich@gmail.com>
2022-07-17 20:53:18 +0500

    Добавьте файлы проекта.

---
+++ Part_1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//1.    Обязательная задача*. Два треугольника заданы длинами своих сторон. Определить, площадь какого из них больше
+//(создать метод для вычисления  площади  треугольника по длинам его сторон).  Для решения задачи можно использовать формулу
+//Герона 
+
+//, где x, y, z – стороны треугольника, p – полупериметр.
+namespace Part_1
+{
+    internal class Program
+    {
+        static void GetSquare(double a, double b, double c, out double s)
+        {
+            double p = (a + b + c) / 2;
+            s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину трех сторон первого треугольника");
+            double a1 = Convert.ToDouble(Console.ReadLine());
+            double b1 = Convert.ToDouble(Console.ReadLine());
+            double c1 = Convert.ToDouble(Console.ReadLine());
+            double s1;
+            GetSquare(a1, b1, c1, out s1);
+            Console.WriteLine("Введите длину трех сторон второго треугольника");
+            double a2 = Convert.ToDouble(Console.ReadLine());
+            double b2 = Convert.ToDouble(Console.ReadLine());
+            double c2 = Convert.ToDouble(Console.ReadLine());
+            double s2;
+            GetSquare(a2, b2, c2, out s2);
+            Console.WriteLine();
+            Console.WriteLine("Площадь первого треугольника = {0:0.00}", s1);
+            Console.WriteLine("Площадь второго треугольника = {0:0.00}", s2);
+            Console.WriteLine();
+            if (s1 >= s2)
+            {
+                if (s1 > s2)
+                    Console.WriteLine("Площадь первого треугольника больше");
+                else
+                    Console.WriteLine("Площади треугольников равны");
+            }
+            if (s1 < s2)
+                    Console.WriteLine("Площадь второго треугольника больше");
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Part_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//2.    Обязательная задача*. Создать метод для вычисления объема и площади поверхности куба по длине его ребра.
+namespace Part_2
+{
+    internal class Program
+    {
+        static void GetCube(double a, out double s, out double v)
+        {
+            s = 6 * a* a; //площадь
+            v = a * a * a; //обьем
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра куба");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double s ;
+            double v;
+            GetCube(a, out s, out v);
+            Console.WriteLine();
+            Console.WriteLine("Площадь куба ={0}", s);
+            Console.WriteLine("Объем куба = {0}", v);
+            Console.ReadLine();
+        }
+    }
+}
